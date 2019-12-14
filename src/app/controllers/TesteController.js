746e4d184f227056eefb36f5ea29894a1da76354
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2019-12-14 07:59:11 -0400

    fix 36

--- src/app/controllers/TesteController.js
+++ src/app/controllers/TesteController.js
@@ -115,7 +115,7 @@ class TesteController {
 				<numero>178148599</numero>
 				<valor>1000</valor>
 				<moeda>986</moeda>
-				<data-hora>2011-12-07T11:43:37</data-hora>
+				<data-hora>2019-12-07T11:43:37</data-hora>
 				<descricao>[origem:10.50.54.156]</descricao>
 				<idioma>PT</idioma>
 				<soft-descriptor/>
