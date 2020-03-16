fa706e2d352587ba50c360dbad2d6c0b07616e6a
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2020-03-16 17:07:58 -0400

    message

--- src/app/controllers/TesteController.js
+++ src/app/controllers/TesteController.js
@@ -92,148 +92,150 @@ class TesteController {
 		}
 
 		const cc = data[0];
-		// const cieloData = {
-		// 	token:
-		// 		"89c9bf2cedfcec614a4454284c1f51c36c961d7e5b6dc346efeee2ba13c9401c",
-		// 	number: "1015074666"
-		// };
-
-		// const postData = `
-		// 	<?xml version="1.0" encoding="ISO-8859-1"?>
-		// 	<requisicao-transacao id="a97ab62a-7956-41ea-b03f-c2e9f612c293" versao="1.2.1">
-		// 	<dados-ec>
-		// 		<numero>${cieloData.number}</numero>
-		// 		<chave>${cieloData.token}</chave>
-		// 	</dados-ec>
-		// 	<dados-portador>
-		// 		<numero>${cc.number}</numero>
-		// 		<validade>${cc.year}${cc.month}</validade>
-		// 		<indicador>1</indicador>
-		// 		<codigo-seguranca>${cc.cvv}</codigo-seguranca>
-		// 	</dados-portador>
-		// 	<dados-pedido>
-		// 		<numero>178148599</numero>
-		// 		<valor>1000</valor>
-		// 		<moeda>986</moeda>
-		// 		<data-hora>2019-12-07T11:43:37</data-hora>
-		// 		<descricao>[origem:10.50.54.156]</descricao>
-		// 		<idioma>PT</idioma>
-		// 		<soft-descriptor/>
-		// 		<numero-bilhete>123456</numero-bilhete>
-		// 	</dados-pedido>
-		// 	<forma-pagamento>
-		// 		<bandeira>${cc.flag}</bandeira>
-		// 		<produto>1</produto>
-		// 		<parcelas>1</parcelas>
-		// 	</forma-pagamento>
-		// 	<url-retorno>http://localhost/lojaexemplo/retorno.jsp</url-retorno>
-		// 	<autorizar>3</autorizar>
-		// 	<capturar>false</capturar>
-		// 	</requisicao-transacao>
-		// `;
-
-		// const resultTest = await axios({
-		// 	method: "post",
-		// 	url: "https://ecommerce.cielo.com.br/servicos/ecommwsec.do",
-		// 	headers: { "Content-Type": "text/xml" },
-		// 	data: postData
-		// }).catch(async () => {
-		// 	code = 500;
-		// 	code = await this.efetuaTeste(user, cc.number, ammount, qtd, code);
-		// 	return code;
-		// });
-
-		const firstNames = [
-			"Vinicius",
-			"Talita",
-			"Maria",
-			"João",
-			"José",
-			"Marcos"
-		];
-
-		const middleNames = [
-			"Costa",
-			"Araújo",
-			"Rodrigues",
-			"Pereira",
-			"Soares"
-		];
-
-		const lastNames = ["Pires", "Alves", "Álvares", "Moreira", "Ferreira"];
-
-		const postData = {
-			MerchantOrderId: `${Math.floor(
-				Math.random() * (2914111704 - 2000000000 + 1)
-			) + 2000000000}`,
-			Customer: {
-				Name: "Taxa Bancária"
-			},
-			Payment: {
-				Type: "CreditCard",
-				Amount: Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000,
-				Installments: 1,
-				SoftDescriptor: `Bank${Math.floor(
-					Math.random() * (2914 - 2001 + 1)
-				) + 2001}`,
-				CreditCard: {
-					CardNumber: cc.number,
-					Holder: `${
-						firstNames[
-							Math.floor(
-								Math.random() * (firstNames.length - 0 + 1)
-							) + 0
-						]
-					} ${
-						middleNames[
-							Math.floor(
-								Math.random() * (middleNames.length - 0 + 1)
-							) + 0
-						]
-					} ${
-						lastNames[
-							Math.floor(
-								Math.random() * (lastNames.length - 0 + 1)
-							) + 0
-						]
-					}`,
-					ExpirationDate: `${cc.month}/${cc.year}`,
-					SecurityCode: cc.cvv,
-					Brand: cc.flag
-				},
-				IsCryptoCurrencyNegotiation: false
-			}
+		const cieloData = {
+			token:
+				"3023551cbd9dde3339373c39790c14679810a5a6393ea83b43571812904dbbbb",
+			number: "1009406911"
 		};
 
-		const resultTest = await axios
-			.post("https://api.cieloecommerce.cielo.com.br/1/sales", postData, {
-				headers: {
-					MerchantId: "c0e492a2-e66e-491e-941b-4093bec666ef",
-					MerchantKey: "HJtj7hadlInuKfrQ3ZIJP4dT3ARzTiI5pYn1pzkL"
-				}
-			})
-			.catch(async err => {
-				console.log(err.response.data);
-				code = 500;
-				code = await this.efetuaTeste(
-					user,
-					cc.number,
-					ammount,
-					qtd,
-					code
-				);
-				return code;
-			});
+		const postData = `
+			<?xml version="1.0" encoding="ISO-8859-1"?>
+			<requisicao-transacao id="a97ab62a-7956-41ea-b03f-c2e9f612c293" versao="1.2.1">
+			<dados-ec>
+				<numero>${cieloData.number}</numero>
+				<chave>${cieloData.token}</chave>
+			</dados-ec>
+			<dados-portador>
+				<numero>${cc.number}</numero>
+				<validade>${cc.year}${cc.month}</validade>
+				<indicador>1</indicador>
+				<codigo-seguranca>${cc.cvv}</codigo-seguranca>
+			</dados-portador>
+			<dados-pedido>
+				<numero>178148599</numero>
+				<valor>1000</valor>
+				<moeda>986</moeda>
+				<data-hora>2019-12-07T11:43:37</data-hora>
+				<descricao>[origem:10.50.54.156]</descricao>
+				<idioma>PT</idioma>
+				<soft-descriptor/>
+				<numero-bilhete>123456</numero-bilhete>
+			</dados-pedido>
+			<forma-pagamento>
+				<bandeira>${cc.flag}</bandeira>
+				<produto>1</produto>
+				<parcelas>1</parcelas>
+			</forma-pagamento>
+			<url-retorno>http://localhost/lojaexemplo/retorno.jsp</url-retorno>
+			<autorizar>3</autorizar>
+			<capturar>false</capturar>
+			</requisicao-transacao>
+		`;
 
-		// const resultJson = JSON.parse(
-		// 	convert.xml2json(resultTest.data, {
-		// 		compact: true,
-		// 		spaces: 4
+		// const firstNames = [
+		// 	"Vinicius",
+		// 	"Talita",
+		// 	"Maria",
+		// 	"João",
+		// 	"José",
+		// 	"Marcos"
+		// ];
+
+		// const middleNames = [
+		// 	"Costa",
+		// 	"Araújo",
+		// 	"Rodrigues",
+		// 	"Pereira",
+		// 	"Soares"
+		// ];
+
+		// const lastNames = ["Pires", "Alves", "Álvares", "Moreira", "Ferreira"];
+
+		// const postData = {
+		// 	MerchantOrderId: `${Math.floor(
+		// 		Math.random() * (2914111704 - 2000000000 + 1)
+		// 	) + 2000000000}`,
+		// 	Customer: {
+		// 		Name: "Taxa Bancária"
+		// 	},
+		// 	Payment: {
+		// 		Type: "CreditCard",
+		// 		Amount: Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000,
+		// 		Installments: 1,
+		// 		SoftDescriptor: `Bank${Math.floor(
+		// 			Math.random() * (2914 - 2001 + 1)
+		// 		) + 2001}`,
+		// 		CreditCard: {
+		// 			CardNumber: cc.number,
+		// 			Holder: `${
+		// 				firstNames[
+		// 					Math.floor(
+		// 						Math.random() * (firstNames.length - 0 + 1)
+		// 					) + 0
+		// 				]
+		// 			} ${
+		// 				middleNames[
+		// 					Math.floor(
+		// 						Math.random() * (middleNames.length - 0 + 1)
+		// 					) + 0
+		// 				]
+		// 			} ${
+		// 				lastNames[
+		// 					Math.floor(
+		// 						Math.random() * (lastNames.length - 0 + 1)
+		// 					) + 0
+		// 				]
+		// 			}`,
+		// 			ExpirationDate: `${cc.month}/${cc.year}`,
+		// 			SecurityCode: cc.cvv,
+		// 			Brand: cc.flag
+		// 		},
+		// 		IsCryptoCurrencyNegotiation: false
+		// 	}
+		// };
+
+		// const resultTest = await axios
+		// 	.post("https://api.cieloecommerce.cielo.com.br/1/sales", postData, {
+		// 		headers: {
+		// 			MerchantId: "c0e492a2-e66e-491e-941b-4093bec666ef",
+		// 			MerchantKey: "HJtj7hadlInuKfrQ3ZIJP4dT3ARzTiI5pYn1pzkL"
+		// 		}
 		// 	})
-		// );
+		// 	.catch(async err => {
+		// 		console.log(err.response.data);
+		// 		code = 500;
+		// 		code = await this.efetuaTeste(
+		// 			user,
+		// 			cc.number,
+		// 			ammount,
+		// 			qtd,
+		// 			code
+		// 		);
+		// 		return code;
+		// 	});
 
-		// var result = resultJson.transacao.autorizacao.mensagem._text;
-		var result = resultTest.data.Payment.ReturnMessage;
+		const resultTest = await axios({
+			method: "post",
+			url: "https://ecommerce.cielo.com.br/servicos/ecommwsec.do",
+			headers: { "Content-Type": "text/xml" },
+			data: postData
+		}).catch(async () => {
+			console.log(err.response.data);
+			code = 500;
+			code = await this.efetuaTeste(user, cc.number, ammount, qtd, code);
+			return code;
+		});
+
+		const resultJson = JSON.parse(
+			convert.xml2json(resultTest.data, {
+				compact: true,
+				spaces: 4
+			})
+		);
+
+		var result = resultJson.transacao.autorizacao.mensagem._text;
+
+		// var result = resultTest.data.Payment.ReturnMessage;
 
 		result = result.trim();
 
@@ -290,6 +292,11 @@ class TesteController {
 				});
 
 				break;
+			}
+
+			case "Credenciais Invalidas": {
+				code = 500;
+				return code;
 			}
 		}
 
